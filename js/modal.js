ef9c421b862d643395e5d355d412da908b02b210
Dima Luzhny <117902450+BlackNinja10735@users.noreply.github.com>
2023-10-07 10:38:02 +0300

    Add work(99,5%)

--- js/modal.js
+++ js/modal.js
@@ -16,22 +16,3 @@
     // document.body.classList.toggle("no-scroll");
 }
 })();
-
-(() => {
-    console.log('start')
-    const refs = {
-    openMenuBtn: document.querySelector("[menu-modal-open]"),
-    closeMenuBtn: document.querySelector("[menu-modal-close]"),
-    menu: document.querySelector("[menu-modal]")
-    };
-
-    refs.openMenuBtn.adddEventListener("click", toggleModal);
-    refs.closeMenuBtn.adddEventListener("click", toggleModal);
-
-    function toggleMenu() {
-        console.log('start')
-    console.log("toggle");
-    refs.menu.classList.toggle("is-hidden");
-    // document.body.classList.toggle("no-scroll");
-}
-})();
